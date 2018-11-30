c5a0b379496e164f3b9361a2d42582b32d5a4ef4
vatsy <vatsy@macs-MacBook-Pro.local>
2018-11-30 20:24:55 +0530

    fcm web and app push notifications

---
+++ src/fcm.js
@@ -0,0 +1,98 @@
+const admin = require("firebase-admin");
+
+let serviceAccount;
+export default class Fcm {
+  constructor(config) {
+    this.config = config;
+    let rootDir = process.mainModule.paths[0].split('server')[0].slice(0, -1);
+    serviceAccount = require(rootDir + '/services/' + this.config.serviceAccountJson);
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: serviceAccount.databaseUrl
+      });
+    }
+  }
+
+  sendAppPushNotification(notificationJson) {
+    return new Promise((resolve, reject) => {
+      var successStatus = true, responseMsg = '';
+      const payload = {
+        notification: {
+          title: notificationJson.notification.title,
+          body: notificationJson.notification.body,
+          clickAction: notificationJson.notification.clickAction,
+          color: notificationJson.notification.color,
+          icon: notificationJson.notification.icon,
+          sound: notificationJson.notification.sound,
+          show_in_foreground: notificationJson.notification.show_in_foreground,
+          priority: notificationJson.notification.priority,
+          content_available: notificationJson.notification.content_available,
+          tag: notificationJson.notification.tag
+        },
+        data: notificationJson.notificationPayload
+      };
+      admin.messaging().sendToDevice(notificationJson.notificationTokens, payload)
+        .then(response => {
+          const notificationResponse = response.results;
+          notificationResponse.map((currResponse) => {
+            if (typeof (currResponse['messageId']) === "undefined" || typeof (currResponse['messageId']) === undefined) {
+              successStatus = false;
+              responseMsg = currResponse['error']['message'];
+            }
+          });
+          if (!successStatus) {
+            reject({
+              "success": false,
+              "message": responseMsg
+            });
+          } else {
+            resolve({
+              "success": true,
+              "body": response
+            });
+          }
+        })
+        .catch(error => {
+          reject({
+            "success": false,
+            "message": error.message
+          })
+        });
+    });
+  }
+
+  sendWebPushNotification(notificationJson) {
+    const payload = {
+      notification: {
+        title: notificationJson.notification.title,
+        body: notificationJson.notification.body,
+        clickAction: notificationJson.notification.clickAction,
+        color: notificationJson.notification.color,
+        icon: notificationJson.notification.icon,
+        sound: notificationJson.notification.sound,
+        show_in_foreground: notificationJson.notification.show_in_foreground,
+        priority: notificationJson.notification.priority,
+        content_available: notificationJson.notification.content_available,
+        tag: notificationJson.notification.tag
+      },
+      data: notificationJson.notificationPayload
+    };
+    return new Promise((resolve, reject) => {
+      admin.messaging().sendToDevice(notificationJson.notificationTokens, payload)
+        .then(response => {
+          resolve({
+            "success": true,
+            "body": response
+          });
+        })
+        .catch(error => {
+          reject({
+            "success": false,
+            "message": error.message
+          })
+        });
+    });
+  }
+
+}
